cc9b94332982c074e47fcd6b557b58ab40158750
ninjaboynaru <thiagoCord755@gmail.com>
2018-05-22 13:45:53 -0400

    Added import extension rule and renamed app.jsx

---
+++ source/js/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return <p>Hello World</p>;
+	}
+}
+
+export default App;
